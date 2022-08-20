4a2bdbaded90a28bc79e6d02121b20174142be20
masud <abdullahalmasud01737906772@gmail.com>
2022-08-20 23:07:51 +0600

    make package

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './Banner';
 import './Home.css';
+import Package from './Package/Package';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Package />
         </div>
     );
 };
---
+++ src/Pages/Home/Package/Package.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Package.css';
+
+const Package = () => {
+    return (
+        <div className='container-xxl pb-5'>
+            <h3 className='text-center p-5 fw-bold'>Package</h3>
+            <div className='package-bg'>
+                <div className='package-dev'>
+
+                </div>
+                <div className='package-dev'>
+
+                </div>
+                <div className='package-dev'>
+
+                </div>
+                <div className='package-dev'>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Package;
